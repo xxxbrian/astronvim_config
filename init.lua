0cc685373d46ba29c921afa31424f6dffdc147d3
Bojin Li <bojinxx@gmail.com>
2024-02-12 13:12:18 +1100

    feat: update colorscheme and expand plugin configurations

--- init.lua
+++ init.lua
@@ -18,7 +18,9 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+  -- colorscheme = "catppuccin",
+  colorscheme = "tokyonight",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,37 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  -- Pack
+  -- Normal Languages
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+
+  -- Text Languages
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+
+  -- Configuration Languages
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.json" },
+
+  -- Frameworks and Tools
+  { import = "astrocommunity.pack.docker" },
+  -- { import = "astrocommunity.pack.vue" }
+
+  -- Utilities
+  { import = "astrocommunity.utility.noice-nvim" },
+
+  -- Theme
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+
+  -- Statusline
+  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
+  -- { import = "astrocommunity.bars-and-lines.lualine" },
 }
